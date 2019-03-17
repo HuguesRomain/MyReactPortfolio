fc9312ad8950e74441e1120b4a6907c076d1f9ef
HuguesRomain <huguesromainpro@gmail.com>
2019-03-17 19:45:04 +0100

    boucle icon nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,10 +4,14 @@ import NavElements from './NavElements'
 
 
 class Navigation extends Component {
+  state = {
+    navDataState: {navData}
+  }
   render () {
+    const navElements = Object.keys(this.state.navDataState.navData.Navigation).map(key => <NavElements key={key} detailsNav={this.state.navDataState.navData.Navigation[key]}></NavElements>)
     return (
       <div className='navigation'>
-        <NavElements/>
+        { navElements }
       </div>
     )
   }
--- src/components/Nav/NavElements.js
+++ src/components/Nav/NavElements.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import navData from '../../data/navData'
 
-class NavElements extends Component {
+/* class NavElements extends Component {
   state = {
     NavElements: {navData}
   }
@@ -10,11 +10,26 @@ class NavElements extends Component {
       <div className='nav__elements'>
         <div className="nav__element">
           <img className='navIcon'  src={require(`../../img/icon/${this.state.NavElements.navData.Navigation.item1.navIcon}`)} />
-          <p>{this.state.NavElements.navData.Navigation.item1.title}</p>
+          <p className='navTitle'>{this.state.NavElements.navData.Navigation.item1.title}</p>
         </div>
       </div>
     )
   }
+}
+
+export default NavElements
+ */
+
+
+const NavElements = ({detailsNav}) => {
+  return (
+  <div className='nav__elements'>
+    <div className="nav__element">
+      <img className='navIcon'  src={require(`../../img/icon/${detailsNav.navIcon}`)} />
+      <p className='navTitle'>{detailsNav.title}</p>
+    </div>
+  </div>
+  )
 }
 
 export default NavElements
