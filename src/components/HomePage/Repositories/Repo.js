2a7287e76556a7a1f32352fa80c42ba563cae16f
HuguesRomain <huguesromainpro@gmail.com>
2019-03-20 15:02:53 +0100

    jsuis un boss

--- src/components/HomePage/Repositories/Repo.js
+++ src/components/HomePage/Repositories/Repo.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 import RepoCards from './RepoCards'
 
 class Repo extends Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+  componentDidMount() {
+    fetch(`https://api.github.com/users/HuguesRomain/repos?access_token=03abad12cf96b6ef86b56a52e6b5260e1c1b0776`)
+      .then(res => res.json())
+      .then(json => this.setState({ data: json }));
+  }
   render () {
+    const repoCards = Object.keys(this.state.data).slice(0, 5).map(key => <RepoCards key={key} detailsRepo={this.state.data[key]}></RepoCards>)
     return (
       <div className='repositories'>
-        <RepoCards/>
+        {repoCards}
       </div>
     )
   }
--- src/components/HomePage/Repositories/RepoCards.js
+++ src/components/HomePage/Repositories/RepoCards.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react'
 
-
-class RepoCards extends Component {
-  state = {
-    test: {},
-  }
-  constructor() {
-    super();
-    this.state = { data: [] };
-  }
-  componentDidMount() {
-    fetch(`https://api.github.com/users/HuguesRomain/repos?access_token=03abad12cf96b6ef86b56a52e6b5260e1c1b0776`)
-      .then(res => res.json())
-      .then(json => this.setState({ test: json }));
-  }
-
-  render () {
-    console.log(this.state.test)
-    return (
-      <div className='repo__card-comingsoon'> 
+const RepoCards = ({detailsRepo}) => {
+  return (
+    <div>
+    <div className='repo__card-comingsoon'> 
         <div className="repo__title"> 
-            
+          {detailsRepo.name}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default RepoCards
