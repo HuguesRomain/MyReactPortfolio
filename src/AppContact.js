017f9842432749a396681c031538c02c5af7ddba
HuguesRomain <huguesromainpro@gmail.com>
2019-03-25 09:26:06 +0100

    form

--- src/AppContact.js
+++ src/AppContact.js
@@ -8,7 +8,35 @@ class AppContact extends Component {
     return (
       <div className="App">
         <div className="page">
-        <div className='content'></div>
+          <div className="intro__contact">
+           <p>want to tell me something ?</p>
+            <p>An advice ? A project ? contact me !</p>
+          </div>
+        <div className='content'>
+        <form className='contact__form'>
+          <div className="form__elements">
+           <input 
+           className='input__name' 
+           placeholder='Your name' 
+           type="text" name="name" 
+           id=""/>
+           <input 
+           className='input__email' 
+           placeholder='Your e-mail' 
+           type="email" name="email" 
+           id=""/>
+           <textarea 
+           className='input__message' 
+           placeholder='Message' 
+           type="text" 
+           name="message" 
+           id=""/>
+          </div>
+          <button 
+          className='button__send'
+          type='submit'>Send !</button>
+        </form>
+        </div>
       </div>
     </div>
     );
